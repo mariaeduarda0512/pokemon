466ae7a36cf106e370ffc087a6cbb90bbd64da05
Maria Eduarda <125601969+mariaeduarda0512@users.noreply.github.com>
2023-07-04 16:53:33 -0300

    Add files via upload

---
+++ server.ts
@@ -0,0 +1,8 @@
+import app from '../app'
+try {
+    app.listen(3001, 'localhost', () => {
+        console.log('Server running at http://localhost:3001');
+    })
+} catch (error) {
+    console.log(error);   
+}
